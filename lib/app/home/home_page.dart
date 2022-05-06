fe1cc8769678d6bd275b001bc240f27a9d6465d0
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-05-05 20:35:57 -0400

    criando botões de categorias

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:podcast_multiplataforma_dio/app/util/custom_color.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -56,7 +61,109 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.only(right: 2, left: 20),
+                height: 35,
+                width: double.infinity,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    CategoryWidget(
+                      text: 'PHP',
+                      color: CustomColor.pink700,
+                    ),
+                    SizedBox(width: 5),
+                    CategoryWidget(
+                      text: 'Dart',
+                      color: CustomColor.pink700,
+                    ),
+                    SizedBox(width: 5),
+                    CategoryWidget(
+                      text: 'Flutter',
+                      color: CustomColor.pink700,
+                    ),
+                    SizedBox(width: 5),
+                    CategoryWidget(
+                      text: 'ReactJS',
+                      color: CustomColor.pink700,
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(25, 30, 25, 0),
+                  child: const Text(
+                    'Podcasts recentes',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: Container(
+                  color: Colors.red,
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        PodcastWidget(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PodcastWidget extends StatelessWidget {
+  const PodcastWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+  
+
+}
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({
+    Key? key,
+    required this.text,
+    required this.color,
+  }) : super(key: key);
+
+  final String text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30,
+      width: 130,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: color,
+      ),
+      child: Center(
+        child: Text(
+          text.toUpperCase(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
           ),
         ),
       ),
