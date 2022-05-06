2f8f35ecddbe0df8631e2846eecf422f387ffa5b
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-05-05 20:06:10 -0400

    configurando logo na appbar

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:podcast_multiplataforma_dio/app/util/custom_color.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,18 +14,47 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget> [
+            children: <Widget>[
               Padding(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 25,
                   vertical: 25,
                 ),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Live Coding Podcast')
+                    Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        SvgPicture.asset(
+                          "assets/logo.svg",
+                          width: 40,
+                          height: 40,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: const [
+                        Text(
+                          "Dio",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          "Cast",
+                          style: TextStyle(
+                            color: CustomColor.pink700,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-              
               ),
             ],
           ),
