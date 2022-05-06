37a8a318b26a7518b55ad3b32f8f616648f41ece
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-05-05 21:38:32 -0400

    finalizando view

---
+++ lib/app/podcast/podcast_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PodcastScreen extends StatefulWidget {
+
+  const PodcastScreen({ Key? key }) : super(key: key);
+
+  @override
+  _PodcastScreenState createState() => _PodcastScreenState();
+}
+
+class _PodcastScreenState extends State<PodcastScreen> {
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
